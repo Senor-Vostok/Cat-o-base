2fe5172c89126125c22b061806c79715e0bbcba4
Senor_Vostok <tabulgatin1@gmail.com>
2024-11-06 19:56:32 +0500

    результат теста

--- run-web.py
+++ run-web.py
@@ -1,7 +1,49 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sqlite3
 
+CatBase = list()
+CatPoints = dict()
+conn = sqlite3.connect('cats.db')
+cursor = conn.cursor()
+cursor.execute('SELECT * FROM cat_breeds')
+users = cursor.fetchall()
+for user in users:
+    CatBase.append(user[1:])
+    CatPoints[user[1]] = 0
+conn.close()
+
 app = Flask(__name__)
+
+criteria = {"size": {"small": ['Корниш-рекс', 'Манчкин'],
+                     'medium': ['Бирманская', 'Японский бобтейл', 'Девон-рекс', 'Корниш-рекс', 'Шотландская вислоухая',
+                                'Абердин', 'Бразильская короткошерстная', 'Петербургский сфинкс', 'Сфинкс',
+                                'Бенгальская', 'Абиссинская', 'Русская голубая', 'Персидская', 'Сиамская',
+                                'Британская короткошерстная'],
+                     'large': ['Турецкий ван', 'Чаузи', 'Норвежская лесная', 'Сибирская', 'Мэйн-кун']},
+            'coat_length': {'short': ['Японский бобтейл', 'Девон-рекс', 'Корниш-рекс', 'Шотландская вислоухая',
+                                      'Чаузи', 'Петербургский сфинкс', 'Сфинкс'],
+                            'medium': ['Абердин', 'Бразильская короткошерстная', 'Манчкин', 'Бенгальская',
+                                       'Абиссинская', 'Русская голубая', 'Сиамская', 'Британская короткошерстная'],
+                            'long': ['Бирманская', 'Турецкий ван', 'Норвежская лесная', 'Сибирская', 'Персидская',
+                                     'Мэйн-кун']},
+            'personality': {'playful': ['Девон-рекс', 'Турецкий ван', 'Чаузи', 'Сфинкс', 'Бенгальская', 'Абиссинская',
+                                        'Сиамская'],
+                            'calm': ['Японский бобтейл', 'Корниш-рекс', 'Петербургский сфинкс', 'Норвежская лесная',
+                                     'Сибирская', 'Манчкин', 'Мэйн-кун'],
+                            'independent': ['Бирманская', 'Шотландская вислоухая', 'Абердин',
+                                            'Бразильская короткошерстная', 'Русская голубая', 'Персидская',
+                                            'Британская короткошерстная']},
+            'affection': {
+                'high': ['Бирманская', 'Японский бобтейл', 'Девон-рекс', 'Турецкий ван', 'Шотландская вислоухая',
+                         'Абердин', 'Бразильская короткошерстная', 'Петербургский сфинкс', 'Сибирская',
+                         'Сфинкс', 'Бенгальская', 'Персидская', 'Мэйн-кун', 'Сиамская', 'Британская короткошерстная'],
+                'moderate': ['Корниш-рекс', 'Чаузи', 'Норвежская лесная', 'Манчкин', 'Абиссинская',
+                             'Русская голубая'],
+                'low': []},
+            'shedding': {'low': ['Мэйн-кун', 'Норвежская лесная', 'Турецкий ван'],
+                         'high': ['Японский бобтейл', 'Девон-рекс', 'Корниш-рекс', 'Шотландская вислоухая',
+                                  'Чаузи', 'Петербургский сфинкс', 'Сфинкс'],
+                         'any': []}}
 
 
 @app.route("/")
@@ -12,7 +54,8 @@ def base():
 @app.route('/extended')
 def base_extended():
     cards = [
-        {'title': CatBase[i][0], 'image': f'{i + 1}.png', 'description': ' '.join(CatBase[i][1:])} for i in range(len(CatBase))
+        {'title': CatBase[i][0], 'image': f'{i + 1}.png', 'description': ' '.join(CatBase[i][1:])} for i in
+        range(len(CatBase))
     ]
     for i in range(len(cards)):
         cards[i]["description"] = ""
@@ -40,13 +83,18 @@ def testing():
     return render_template('test.html')
 
 
+@app.route('/result', methods=['POST'])
+def result():
+    points = CatPoints
+    for crit in criteria:
+        for cat in criteria[crit][request.form.get(crit)]:
+            points[cat] += 1
+    breed = (sorted([i for i in points], key=lambda x: points[x]))
+    print(breed)
+    for i in CatPoints:
+        CatPoints[i] = 0
+    return render_template('result.html', breed=breed[0])
+
+
 if __name__ == '__main__':
-    CatBase = list()
-    conn = sqlite3.connect('cats.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM cat_breeds')
-    users = cursor.fetchall()
-    for user in users:
-        CatBase.append(user[1:])
-    conn.close()
     app.run()
